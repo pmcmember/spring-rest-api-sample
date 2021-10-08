a83f57352f51d048e5cb09256555ec21d705f930
dev-fjk <87862521+dev-fjk@users.noreply.github.com>
2021-10-09 00:18:22 +0900

    Initial commit

---
+++ src/main/java/com/gradle/example/gradlebase/GradleBaseApplication.java
@@ -0,0 +1,12 @@
+package com.gradle.example.gradlebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleBaseApplication.class, args);
+    }
+}
