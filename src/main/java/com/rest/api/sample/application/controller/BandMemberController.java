fb9be12fcaaad280283af62bf27c986c20d28b2d
dev-fjk <fjk.develop@gmail.com>
2021-10-10 16:23:55 +0900

    uri修正

--- src/main/java/com/rest/api/sample/application/controller/BandMemberController.java
+++ src/main/java/com/rest/api/sample/application/controller/BandMemberController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
-@RequestMapping(value = "api/v1/band")
+@RequestMapping(value = "api/v1/band/")
 @RequiredArgsConstructor
 public class BandMemberController {
 
